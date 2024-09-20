0ea0bcdf3619a296e35f9b11ebd34128edcc5617
jose ramon <jazz_crack@hotmail.com>
2024-09-20 09:21:17 +0200

    filtros de busqueda

--- src/components/Pais.jsx
+++ src/components/Pais.jsx
@@ -12,12 +12,29 @@ const Pais = () => {
     // omitir si el pais no está definido
     if (name) {
       console.log('Nuevo pais', name)
-      axios
-        .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
-        .then(response => {
-          setCountry(response.data)
-        })
+      if (name) {
+        console.log('Buscando países que coincidan con:', name);
+        axios.get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+          .then(response => {
+            const filteredCountries = response.data.filter(country => 
+              country.name.common.toLowerCase().includes(name.toLowerCase())
+            );
+            
+            console.log('Países encontrados:', filteredCountries);
+      
+            if (filteredCountries.length > 10) {
+              setCountry({ error: 'Demasiados países encontrados. Por favor, especifica más.' });
+            } else if (filteredCountries.length > 1) {
+              setCountry(filteredCountries); // Muestra todos los países si hay entre 2 y 10 resultados
+            } else {
+              setCountry(filteredCountries[0]); // Solo un país, muestra ese directamente
+            }
+          })
+          .catch(error => console.error('Error al buscar países:', error));
+      }
+      
     }
+
   }, [name])
 
   const handleChange = (event) => {
